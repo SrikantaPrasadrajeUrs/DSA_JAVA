0195e76e861be33f5b207ae541675853530ff0b0
SrikantaPrasadrajeUrs <srikantaprasadrajeurs456@gmail.com>
2025-08-19 09:27:53 +0530

    updated code

--- data_struc_impl/queue/arraay_list_impl.java
+++ data_struc_impl/queue/arraay_list_impl.java
@@ -7,7 +7,7 @@ class CustomArrayList<T>{
     private static final int DEFAULT_SIZE = 10;
 
     public int length() {
-        return size+1;
+        return size;
     }
 
     CustomArrayList(){
@@ -21,17 +21,53 @@ class CustomArrayList<T>{
     }
 
     public void add(T value){
-        if(checkIndex(size)){
-            array[size] = value;
-            size++;
+        if(checkCapacity()){
+            array[size++] = value;
 ;        }else{
             ensureCapacity();
-            array = Arrays.copyOf(array, capacity);
+            enlargeArray();
             array[size++] = value;
         }
     }
 
+    void enlargeArray(){
+        array = Arrays.copyOf(array, capacity);
+//        System.out.println("size increased, "+Arrays.toString(array));
+    }
+
+    public void add(T value, boolean shouldCheck){
+        System.out.println("size: "+size+" value: "+value);
+        if(shouldCheck) array[size++] = value;
+        else add(value);
+    }
+
+    boolean isEmpty(){
+        return size==0;
+    }
+
+    boolean contains(Object value){
+        for(int i=0;i<size;i++){
+            if(array[i].equals(value)) return true;
+        }
+        return false;
+    }
+
+    int indexOf(Object value){
+        int index = -1;
+        for(int i=0;i<size;i++){
+            if(array[i].equals(value)){
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
     public void clear(){
+        // to avoid memory leaks
+        for(int i=0;i<size;i++){
+            array[i] = null;
+        }
         array = (T[]) new Object[DEFAULT_SIZE];
         size = 0;
         capacity = DEFAULT_SIZE;
@@ -39,35 +75,57 @@ class CustomArrayList<T>{
 
     public void insertAt(int index, T value){
         if(!checkIndex(index)) throw new ArrayIndexOutOfBoundsException();
+        for(int i=index;i<size-1;i++){
+            array[i+1] = array[i];
+        }
         array[index] = value;
     }
 
     public void delete(int index){
        if(!checkIndex(index)) throw new ArrayIndexOutOfBoundsException();
-       for(int i=index;i< array.length-1;i++){
+       for(int i=index;i< size-1;i++){
            array[i] = array[i+1];
        }
-       size--;
+       array[--size] = null;
     }
 
     private void ensureCapacity(){
         if(size+1>=capacity)capacity+=DEFAULT_SIZE;
     }
 
+    private void ensureCapacity(boolean shouldIncrease){
+        if(shouldIncrease)capacity+=DEFAULT_SIZE;
+    }
+
     private boolean checkIndex(int index){
-        return index < capacity && index >= 0;
+        return index < size && index >= 0;
+    }
+
+    private boolean checkCapacity(){
+        return size<capacity&&size>=0;
     }
 
     public void addAll(CustomArrayList<T> customArrayList){
-         int length = customArrayList.length();
-
+         int length = customArrayList.length()-1;
+         boolean didCapacityModified = false;
+        while(!checkIndex(size+length)){
+            didCapacityModified = true;
+            System.out.println(capacity);
+            ensureCapacity(true);
+        }
+        if(didCapacityModified)enlargeArray();
+        for(int i=0;i<length;i++){
+            add(customArrayList.array[i],true);
+        }
     }
 
     public static void main(String[] args){
         CustomArrayList<Integer> arrayList = new CustomArrayList<>();
+        CustomArrayList<Integer> arrayList2 = new CustomArrayList<>();
         arrayList.add(10);
         arrayList.add(20);
         arrayList.add(30);
+//        arrayList.clear();
         arrayList.add(40);
         arrayList.add(50);
         arrayList.add(60);
@@ -81,7 +139,14 @@ class CustomArrayList<T>{
         arrayList.delete(10);
         System.out.println(arrayList);
         System.out.println(arrayList.get(0));
-        arrayList.clear();
+        arrayList2.add(10);
+        arrayList2.add(44);
+        arrayList2.add(80);
+        arrayList2.add(90);
+        arrayList2.add(100);
+        arrayList2.add(11);
+        arrayList.add(12);
+        arrayList2.addAll(arrayList2);
         System.out.println(arrayList);
     }
 
